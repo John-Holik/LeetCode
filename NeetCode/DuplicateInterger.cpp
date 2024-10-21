86fc3f6ca37ae9ed69be97e7dcc4f7d366e2d6f6
John-Holik <johnholikiii@outlook.com>
2024-10-21 15:04:26 -0400

    Reformatting Brackets

--- NeetCode/DuplicateInterger.cpp
+++ NeetCode/DuplicateInterger.cpp
@@ -6,24 +6,18 @@ My Solution:  */
 
 using namespace std;
 
-class Solution
-{
+class Solution{
 
 public:
-    bool hasDuplicate(vector<int> &nums)
-    {
+    bool hasDuplicate(vector<int> &nums){
         size_t size = nums.size();
-        if (size == 0)
-        {
+        if (size == 0){
             return false;
         }
 
-        for (int i = 0; i < size; i++)
-        {
-            for (int j = i + 1; j < size; j++)
-            {
-                if (nums[i] == nums[j])
-                {
+        for (int i = 0; i < size; i++){
+            for (int j = i + 1; j < size; j++){
+                if (nums[i] == nums[j]){
                     return true;
                 }
             }
